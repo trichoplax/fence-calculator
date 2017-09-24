41e6651a55d27e936d1f5b8475c185a84582e6fb
Robbie Wood <githubphagocyte@gmail.com>
2017-09-24 19:40:06 +0100

    Add reverse functionality - length given materials.

--- index.php
+++ index.php
@@ -1,22 +1,64 @@
 <?php
+define('POST_WIDTH', 0.1);
+define('RAILING_LENGTH', 1.5);
 require_once('php/functions.php')
 ?>
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Fence Calculator</title>
+</head>
 <body>
 
 <H1>Fence Calculator</H1>
 
-<form action="index.php" method="post">
-    Required fence length: <input type="number" name="length" required>
-    <input type="submit" name="submit">
-</form><br>
+<hr>
 
-<?php echo messageToUser() ?>
+<form action="index.php" method="post">
+    <label for="length">
+        Required fence length: <input type="number" name="length" required><br>
+    </label>
+    <input type="submit" name="length-submit">
+</form>
+
+<?php
+if (isset($_POST['length-submit'])) {
+    echo messageFromLength($_POST['length'],
+        RAILING_LENGTH,
+        POST_WIDTH);
+} else {
+    echo 'Specify a length, then press "Submit".';
+}?>
+
+<hr>
+
+<form action="index.php" method="post">
+    Available materials:<br>
+    <label for="posts">
+        Posts: <input type="number" name="posts" required><br>
+    </label>
+    <label for="railings">
+        Railings: <input type="number" name="railings" required><br>
+    </label>
+    <input type="submit" name="materials-submit">
+</form>
+
+<?php
+if (isset($_POST['materials-submit'])) {
+    echo messageFromMaterials($_POST['posts'],
+        $_POST['railings'],
+        POST_WIDTH,
+        RAILING_LENGTH);
+} else {
+    echo 'Specify how many available posts and railings, then press "Submit".';
+}
+?>
+
+<hr>
 
 <footer>
-    <p>Post width: <?php echo POST_WIDTH; ?>m.</p>
-    <p> Railing length: <?php echo RAILING_LENGTH; ?>m.</p>
+    Post width: <?php echo POST_WIDTH; ?>m.
+    Railing length: <?php echo RAILING_LENGTH; ?>m.
 </footer>
 
 </body>
--- php/functions.php
+++ php/functions.php
@@ -1,23 +1,25 @@
 <?php
-define('POST_WIDTH', 0.1);
-define('RAILING_LENGTH', 1.5);
-
 /**
- * Calculate the posts and railings required.
+ * Calculate posts and railings required.
  *
- * @param $length The required length of fence.
+ * @param float $length Length of required fence.
+ * @param float $postWidth Width of a post.
+ * @param float $railingLength Length of a railing.
  *
- * @return string A description of the required posts and railings.
+ * @return string Description of the posts and railings required.
  */
-function requirements($length)
+function materialsGivenLength(float $length,
+                              float $postWidth,
+                              float $railingLength)
 {
-    $railings = ceil(($length - POST_WIDTH) / (RAILING_LENGTH + POST_WIDTH));
+    $railings = ceil(($length - $postWidth) / ($postWidth + $railingLength));
     $posts = $railings + 1;
-    $total = $railings * RAILING_LENGTH + $posts * POST_WIDTH;
+    $total =  $posts * $postWidth + $railings * $railingLength;
     $overshoot = $total - $length;
     $returnText = "";
     $returnText .= "For a fence of length " . $length . " metres, ";
-    $returnText .= "you will require " . $posts . " posts and " . $railings . " railings.<br>";
+    $returnText .= "you will require " . $posts . " posts and ";
+    $returnText .= $railings . " railings.<br>";
     $returnText .= 'This will give a fence of length ' . $total . ' metres';
     if ($overshoot > 0)
     {
@@ -29,16 +31,63 @@ function requirements($length)
 }
 
 /**
- * Decide which text to display to the user.
+ * Calculate possible length of fence.
  *
- * @return string Either requirements or instructions if no input yet.
+ * @param int $posts The number of posts available.
+ * @param int $railings The number of railings available.
+ * @param float $postWidth The width of a post.
+ * @param float $railingLength The length of a railing.
+ *
+ * @return string Description of the length of fence possible.
  */
-function messageToUser()
+function lengthGivenMaterials(int $posts, int $railings, float $postWidth, float $railingLength)
 {
-    if (isset($_POST['submit'])) {
-        $summary = requirements($_POST['length']);
+    $usableRailings = min($railings, $posts - 1);
+    $usablePosts = $usableRailings + 1;
+    $length = $usableRailings * ($railingLength + $postWidth) + $postWidth;
+    $surplusRailings = $railings - $usableRailings;
+    $surplusPosts = $posts - $usablePosts;
+    $returnText = '';
+    $returnText .= 'Using ' . $usablePosts . ' posts and ' . $usableRailings;
+    $returnText .= ' railings you can build a fence of length ' . $length;
+
+    if ($surplusPosts > 0) {
+        $returnText .= ', leaving ' . $surplusPosts . ' posts left over.';
+    } elseif ($surplusRailings > 0) {
+        $returnText .= ', leaving ' . $surplusRailings . ' railings left over.';
     } else {
-        $summary = 'Specify a length and then press "Submit".';
+        $returnText .= '.';
     }
+    return $returnText;
+}
+
+/**
+ * Display a message based on the posts and railings specified.
+ *
+ * @param int $posts The number of posts available.
+ * @param int $railings The number of railings available.
+ * @param float $postWidth The width of a post.
+ * @param float $railingLength The length of a railing.
+ *
+ * @return string Description of length or else instructions.
+ */
+function messageFromMaterials(int $posts, int $railings, float $postWidth, float $railingLength)
+{
+    $summary = lengthGivenMaterials($posts, $railings, $postWidth, $railingLength);
+    return $summary;
+}
+
+/**
+ * Display a message based on the length specified.
+ *
+ * @param float $length The length of fence required.
+ * @param float $postWidth The width of a post.
+ * @param float $railingLength The length of a railing.
+ *
+ * @return string Description of materials or else instructions.
+ */
+function messageFromLength(float $length, float $postWidth, float $railingLength)
+{
+        $summary = materialsGivenLength($length, $postWidth, $railingLength);
     return $summary;
 }
