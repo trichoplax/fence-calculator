66c111260b7e81764f23daa9f9040ae8dd2e6992
Robbie Wood <githubphagocyte@gmail.com>
2017-09-24 21:12:20 +0100

    Add validation.

--- php/functions.php
+++ php/functions.php
@@ -25,8 +25,7 @@ function messageFromMaterials(int $posts,
         $length = round($usableRailings * ($railingLength + $postWidth) + $postWidth, 2);
         $surplusRailings = $railings - $usableRailings;
         $surplusPosts = $posts - $usablePosts;
-        $returnText = '';
-        $returnText .= 'Given ' . $posts . ' posts and ' . $railings;
+        $returnText = 'Given ' . $posts . ' posts and ' . $railings;
         $returnText .= ' railings:<br>You can use ' . $usablePosts;
         $returnText .= ' posts and ' . $usableRailings;
         $returnText .= ' railings to build a fence of length ';
@@ -56,20 +55,25 @@ function messageFromLength(float $length,
                            float $postWidth,
                            float $railingLength)
 {
-    $railings = ceil(($length - $postWidth) / ($postWidth + $railingLength));
-    $posts = $railings + 1;
-    $total =  round($posts * $postWidth + $railings * $railingLength, 2);
-    $overshoot = round($total - $length, 2);
-    $returnText = "";
-    $returnText .= "For a fence of length " . $length . " metres, ";
-    $returnText .= "you will require " . $posts . " posts and ";
-    $returnText .= $railings . " railings.<br>";
-    $returnText .= 'This will give a fence of length ' . $total . ' metres';
-    if ($overshoot > 0)
-    {
-        $returnText .= ', ' . $overshoot . ' metres longer than required.';
+    if ($length == 0) {
+        $returnText = 'For a length of zero you do not require a fence.';
+    } elseif ($length < 0) {
+        $returnText = 'You cannot have a negative length fence. ';
+        $returnText .= 'We are out of negative length railings.';
     } else {
-        $returnText .= '.';
+        $railings = ceil(($length - $postWidth) / ($postWidth + $railingLength));
+        $posts = $railings + 1;
+        $total = round($posts * $postWidth + $railings * $railingLength, 2);
+        $overshoot = round($total - $length, 2);
+        $returnText = 'For a fence of length ' . $length . ' metres, ';
+        $returnText .= 'you will require ' . $posts . ' posts and ';
+        $returnText .= $railings . ' railings.<br>';
+        $returnText .= 'This will give a fence of length ' . $total . ' metres';
+        if ($overshoot > 0) {
+            $returnText .= ', ' . $overshoot . ' metres longer than required.';
+        } else {
+            $returnText .= '.';
+        }
     }
     return $returnText;
 }
