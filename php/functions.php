194d22ad53ae2bd30204bb80940f9c733885c4c7
Robbie Wood <githubphagocyte@gmail.com>
2017-09-24 20:14:54 +0100

    Eliminate redundant functions and fix precision.

--- php/functions.php
+++ php/functions.php
@@ -1,21 +1,65 @@
 <?php
+
 /**
- * Calculate posts and railings required.
+ * Display a message based on the posts and railings specified.
  *
- * @param float $length Length of required fence.
- * @param float $postWidth Width of a post.
- * @param float $railingLength Length of a railing.
+ * @param int $posts The number of posts available.
+ * @param int $railings The number of railings available.
+ * @param float $postWidth The width of a post.
+ * @param float $railingLength The length of a railing.
  *
- * @return string Description of the posts and railings required.
+ * @return string Description of length or else instructions.
  */
-function materialsGivenLength(float $length,
+function messageFromMaterials(int $posts,
+                              int $railings,
                               float $postWidth,
                               float $railingLength)
 {
+    if ($railings < 1) {
+        $returnText = 'You cannot build a fence with less than 1 railing.';
+    } elseif ($posts < 2) {
+        $returnText = 'You cannot build a fence with less than 2 posts.';
+    } else {
+        $usableRailings = min($railings, $posts - 1);
+        $usablePosts = $usableRailings + 1;
+        $length = round($usableRailings * ($railingLength + $postWidth) + $postWidth, 2);
+        $surplusRailings = $railings - $usableRailings;
+        $surplusPosts = $posts - $usablePosts;
+        $returnText = '';
+        $returnText .= 'Given ' . $posts . ' posts and ' . $railings;
+        $returnText .= ' railings:<br>You can use ' . $usablePosts;
+        $returnText .= ' posts and ' . $usableRailings;
+        $returnText .= ' railings to build a fence of length ';
+        $returnText .= $length . ' metres';
+
+        if ($surplusPosts > 0) {
+            $returnText .= ', with ' . $surplusPosts . ' posts left over.';
+        } elseif ($surplusRailings > 0) {
+            $returnText .= ', with ' . $surplusRailings . ' railings left over.';
+        } else {
+            $returnText .= '.';
+        }
+    }
+    return $returnText;
+}
+
+/**
+ * Display a message based on the length specified.
+ *
+ * @param float $length The length of fence required.
+ * @param float $postWidth The width of a post.
+ * @param float $railingLength The length of a railing.
+ *
+ * @return string Description of materials or else instructions.
+ */
+function messageFromLength(float $length,
+                           float $postWidth,
+                           float $railingLength)
+{
     $railings = ceil(($length - $postWidth) / ($postWidth + $railingLength));
     $posts = $railings + 1;
-    $total =  $posts * $postWidth + $railings * $railingLength;
-    $overshoot = $total - $length;
+    $total =  round($posts * $postWidth + $railings * $railingLength, 2);
+    $overshoot = round($total - $length, 2);
     $returnText = "";
     $returnText .= "For a fence of length " . $length . " metres, ";
     $returnText .= "you will require " . $posts . " posts and ";
@@ -28,66 +72,4 @@ function materialsGivenLength(float $length,
         $returnText .= '.';
     }
     return $returnText;
-}
-
-/**
- * Calculate possible length of fence.
- *
- * @param int $posts The number of posts available.
- * @param int $railings The number of railings available.
- * @param float $postWidth The width of a post.
- * @param float $railingLength The length of a railing.
- *
- * @return string Description of the length of fence possible.
- */
-function lengthGivenMaterials(int $posts, int $railings, float $postWidth, float $railingLength)
-{
-    $usableRailings = min($railings, $posts - 1);
-    $usablePosts = $usableRailings + 1;
-    $length = $usableRailings * ($railingLength + $postWidth) + $postWidth;
-    $surplusRailings = $railings - $usableRailings;
-    $surplusPosts = $posts - $usablePosts;
-    $returnText = '';
-    $returnText .= 'Using ' . $usablePosts . ' posts and ' . $usableRailings;
-    $returnText .= ' railings you can build a fence of length ' . $length;
-
-    if ($surplusPosts > 0) {
-        $returnText .= ', leaving ' . $surplusPosts . ' posts left over.';
-    } elseif ($surplusRailings > 0) {
-        $returnText .= ', leaving ' . $surplusRailings . ' railings left over.';
-    } else {
-        $returnText .= '.';
-    }
-    return $returnText;
-}
-
-/**
- * Display a message based on the posts and railings specified.
- *
- * @param int $posts The number of posts available.
- * @param int $railings The number of railings available.
- * @param float $postWidth The width of a post.
- * @param float $railingLength The length of a railing.
- *
- * @return string Description of length or else instructions.
- */
-function messageFromMaterials(int $posts, int $railings, float $postWidth, float $railingLength)
-{
-    $summary = lengthGivenMaterials($posts, $railings, $postWidth, $railingLength);
-    return $summary;
-}
-
-/**
- * Display a message based on the length specified.
- *
- * @param float $length The length of fence required.
- * @param float $postWidth The width of a post.
- * @param float $railingLength The length of a railing.
- *
- * @return string Description of materials or else instructions.
- */
-function messageFromLength(float $length, float $postWidth, float $railingLength)
-{
-        $summary = materialsGivenLength($length, $postWidth, $railingLength);
-    return $summary;
 }
