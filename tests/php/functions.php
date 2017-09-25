3e38d6b28c58a6f053fc2e166035b1e1d0547f01
Robbie Wood <githubphagocyte@gmail.com>
2017-09-25 11:07:28 +0100

    Add success test for messageFromMaterials.

--- tests/php/functions.php
+++ tests/php/functions.php
@@ -4,7 +4,14 @@ use PHPUnit\Framework\TestCase;
 require_once('../../php/functions.php');
 
 class StackTest extends TestCase {
-    public function testmessageFromMaterialsSuccessMaxTwoDecimalPlaces() {}
+    public function testmessageFromMaterialsSuccessMaxTwoDecimalPlaces() {
+        $case = messageFromMaterials(999, 998, 0.1, 1.5);
+        $expected = 'Given 999 posts and 998 railings:<br>You can use 999';
+        $expected .= ' posts and 998 railings to build a fence of length ';
+        $expected .= '1596.9 metres';
+        $this->assertEquals($case, $expected);
+    }
+
     public function testmessageFromMaterialsFailureNegativePosts() {}
     public function testmessageFromMaterialsFailureNegativeRailings() {}
     public function testmessageFromMaterialsFailureZeroPosts() {}
@@ -15,7 +22,14 @@ class StackTest extends TestCase {
     public function testmessageFromMaterialsMalformedPostsAsNonIntegerFloat() {}
     public function testmessageFromMaterialsMalformedRailingsAsNonIntegerFloat() {}
 
-    public function testmessageFromLengthSuccessShortLength() {}
+    public function testmessageFromLengthSuccessShortLength() {
+        $case = messageFromLength(0.01, 0.1, 1.5);
+        $expected = 'For a fence of length 0.01 metres, you will require 2 posts and ';
+        $expected .= '1 railings.<br>This will give a fence of length 1.7 metres';
+        $expected .= ', 1.69 metres longer than required.';
+        $this->assertEquals($case, $expected);
+    }
+
     public function testmessageFromLengthFailureNegativeLength() {}
     public function testmessageFromLengthFailureZeroLength() {}
     public function testmessageFromLengthMalformedAsArray() {}
